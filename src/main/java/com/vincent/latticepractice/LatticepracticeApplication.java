489df327564c3a91feb5449e8685e26a2e536049
director-D <lookvincent@163.com>
2023-06-23 01:34:07 +0800

    init

---
+++ src/main/java/com/vincent/latticepractice/LatticepracticeApplication.java
@@ -0,0 +1,13 @@
+package com.vincent.latticepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatticepracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatticepracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vincent/latticepractice/LatticepracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.latticepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatticepracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
